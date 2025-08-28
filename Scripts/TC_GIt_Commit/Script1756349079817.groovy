79624cb95b3c278d7384402019f23de075bc3e78
JeanCarlosDev01 <jeancarlosordonez.sena@gmail.com>
2025-08-27 22:08:44 -0500

    Mi primer commit

--- Scripts/TC_GIt_Commit/Script1756349079817.groovy
+++ Scripts/TC_GIt_Commit/Script1756349079817.groovy
@@ -17,3 +17,11 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
+WebUI.openBrowser('https://www.qa-practice.com/')
+
+WebUI.maximizeWindow()
+
+WebUI.click(findTestObject('Home_Page/btn_TextInput'))
+
+WebUI.setText(findTestObject('Input_Page/inptText_TextString'), 'This is a test')
+
