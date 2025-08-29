c06b90fbd9a04d8a50d2b4f20559bba4fc6790ca
JeanCarlosDev01 <jeancarlosordonez.sena@gmail.com>
2025-08-28 20:21:25 -0500

    Actualizacion caso jean

--- Scripts/TC_Jean_User_rename/Script1756430380617.groovy
+++ Scripts/TC_Jean_User_rename/Script1756430380617.groovy
@@ -17,8 +17,3 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-WebUI.openBrowser('')
-
-WebUI.waitForAlert(4)
-
-WebUI.delay(2)
